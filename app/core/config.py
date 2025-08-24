ea4812f55069defa18b9214d3851f445af3597c8
Taciano Da Hora <taciano.dev@outlook.com>
2025-08-24 20:17:02 -0300

    chore: add crossfire variables to API settings

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,13 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "SafeReport API"
     API_V1_STR: str = "/api/v1"
+    EMAIL_CROSSFIRE_API: str
+    PASSWORD_CROSSFIRE_API: str
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
