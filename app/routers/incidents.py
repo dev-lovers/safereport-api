da04076dd08f177ec609f149b295aa252a9890ae
Taciano Da Hora <taciano.dev@outlook.com>
2025-08-30 12:00:22 -0300

    feat: retrieve the user's city and state ID, so that I can use them to retrieve the occurrences

--- app/routers/incidents.py
+++ app/routers/incidents.py
@@ -8,9 +8,20 @@ from app.services import crossfire_api
 
 router = APIRouter(prefix="/incidents")
 
+from pydantic import BaseModel
+
+
+class LocationSchema(BaseModel):
+    country: str
+    state: str
+    city: str
+    street: Optional[str] = None
+    neighborhood: Optional[str] = None
+
 
 @router.post("/hotspots")
 async def get_incident_hotspots(
+    location: LocationSchema,
     response: Response,
     access_token: Optional[str] = Cookie(None),
 ):
@@ -24,6 +35,7 @@ async def get_incident_hotspots(
         if access_token is None:
             email = settings.EMAIL_CROSSFIRE_API
             password = settings.PASSWORD_CROSSFIRE_API
+
             access_token = crossfire_api.get_auth_token(email, password)
 
             if access_token == "":
@@ -40,6 +52,29 @@ async def get_incident_hotspots(
                 samesite="strict",
                 max_age=60 * 60 * 24,
             )
+
+        state_id = crossfire_api.get_state_id(location.state, access_token)
+
+        if state_id is None:
+            raise HTTPException(
+                status_code=404,
+                detail=f"Estado '{location.state}' não encontrado na API do CrossFire.",
+            )
+
+        # TODO: No endpoint da cidade, fornece o ID do estado. Verificar se não é mais interessante usar apenas esse endpoint para buscar ambas as infromações.
+        city_id = crossfire_api.get_city_id(location.city, access_token)
+
+        if city_id is None:
+            raise HTTPException(
+                status_code=404,
+                detail=f"Cidade '{location.city}' não encontrado na API do CrossFire.",
+            )
+
+        # TODO: Incidente ou Ocorrência? Verificar o termo mais adequado. Ocorrência está ganhando por enquanto.
+        occurrences = crossfire_api.get_occurrences(
+            state_id=state_id, city_id=city_id, access_token=access_token
+        )
+        print(occurrences)
 
     except httpx.HTTPStatusError as exc:
         # Se o erro foi um status HTTP (ex: 404 Not Found),
--- app/services/crossfire_api.py
+++ app/services/crossfire_api.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import httpx
 
 CROSSFIRE_API_BASE_URL = "https://api-service.fogocruzado.org.br/api/v2"
@@ -41,7 +43,123 @@ def get_auth_token(email, password):
 
         response.raise_for_status()
         response_data = response.json()
+        print(response_data)
         data_payload = response_data.get("data", {})
         access_token = data_payload.get("accessToken", "")
 
         return access_token
+
+
+def get_state_id(state: str, access_token: str) -> Optional[str]:
+    # TODO: Ajustar docstring
+    """
+    Get auth token from the auth API
+
+    Raises:
+        httpx.HTTPStatusError: Se a API externa retornar um erro (4xx, 5xx).
+        httpx.RequestError: Se houver um problema de conexão com a API.
+    """
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json",
+    }
+
+    with httpx.Client() as client:
+        response = client.get(
+            f"{CROSSFIRE_API_BASE_URL}/states",
+            headers=headers,
+            timeout=10.0,
+        )
+
+        response.raise_for_status()
+        response_data = response.json()
+        data_payload = response_data.get("data", [])
+
+        for state_info in data_payload:
+            state_name = state_info.get("name").lower()
+            if state_name == state.lower():
+                return state_info.get("id")
+
+        return None
+
+
+def get_city_id(city: str, access_token: str) -> Optional[str]:
+    # TODO: Ajustar docstring
+    """
+    Get auth token from the auth API
+
+    Raises:
+        httpx.HTTPStatusError: Se a API externa retornar um erro (4xx, 5xx).
+        httpx.RequestError: Se houver um problema de conexão com a API.
+    """
+    city = city.upper()
+
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json",
+    }
+
+    params = {"cityName": city}
+
+    with httpx.Client() as client:
+        response = client.get(
+            f"{CROSSFIRE_API_BASE_URL}/cities",
+            headers=headers,
+            params=params,
+            timeout=10.0,
+        )
+
+        response.raise_for_status()
+        response_data = response.json()
+        data_payload = response_data.get("data", [])
+
+        for city_info in data_payload:
+            city_name = city_info.get("name")
+            if city_name == city:
+                return city_info.get("id")
+
+        return None
+
+
+def get_occurrences(state_id: str, city_id: str, access_token: str) -> Optional[str]:
+    # TODO: Ajustar docstring
+    """
+    Get auth token from the auth API
+
+    Raises:
+        httpx.HTTPStatusError: Se a API externa retornar um erro (4xx, 5xx).
+        httpx.RequestError: Se houver um problema de conexão com a API.
+    """
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json",
+    }
+
+    # order=ASC&page=1&take=20&idState=813ca36b-91e3-4a18-b408-60b27a1942ef&idCities=d79d2347-bd0d-40aa-8dcc-04134cffd988&idCities=e37f7ad7-cd64-4279-946a-8d689b9b934b
+    params = {
+        "order": "ASC",
+        "page": 1,
+        "take": 1,
+        "idState": state_id,
+        "idCities": city_id,
+    }
+
+    with httpx.Client() as client:
+        response = client.get(
+            f"{CROSSFIRE_API_BASE_URL}/occurrences",
+            headers=headers,
+            params=params,
+            timeout=10.0,
+        )
+
+        response.raise_for_status()
+        response_data = response.json()
+
+        response_code = response_data.get("code")
+
+        # TODO: Verificar se eu devo retornar None ou uma lista vazia
+        if response_code != 200:
+            return None
+
+        data_payload = response_data.get("data", [])
+        return data_payload
